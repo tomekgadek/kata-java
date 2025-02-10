7d16b931a5feb1326161dcfa0ddfd2fde0ad2c5e
Tomasz Gadek <gadektomek@gmail.com>
2025-02-10 22:13:54 +0100

    kata-java, macierz symetryczna (testy)

---
+++ lab-project/src/main/test/groovy/pl/unittest/third_lab/SymmetricMatrixSpec.groovy
@@ -0,0 +1,29 @@
+package pl.unittest.third_lab
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class SymmetricMatrixSpec extends Specification {
+
+    @Unroll
+    def "should correctly set and get matrix values"(int row, int col, int value) {
+        given: "A symmetric matrix of size 3"
+        def matrix = new SymmetricMatrix(3)
+
+        when: "Setting values in the matrix"
+        matrix.set(row, col, value)
+
+        then: "Values should be retrieved correctly"
+        matrix.get(row, col) == value
+        matrix.get(col, row) == value // Symmetric property
+
+        where:
+        row | col | value
+        1   | 1   | 2
+        2   | 2   | 6
+        3   | 3   | 9
+        3   | 1   | 3
+        2   | 3   | 7
+        1   | 2   | 1
+    }
+}
