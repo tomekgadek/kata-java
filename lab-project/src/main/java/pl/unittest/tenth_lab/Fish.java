bca71d9bb0f2d0107a25eff6c7c65717dcbab94d
tomekgadek <gadektomek@gmail.com>
2025-05-24 22:45:54 +0200

    kata-java, przygotowanie tresci zadania (fish)

---
+++ lab-project/src/main/java/pl/unittest/tenth_lab/Fish.java
@@ -0,0 +1,15 @@
+package pl.unittest.tenth_lab;
+
+/*
+    Zadanie polega na napisaniu programu, który wygeneruje kolekcję rybek akwariowych. Klasa Fish ma zawierać co
+    najmniej takie informacje jak: Nazwa, nazwa łacińska, długość, temperatura minimalna wody, temperatura maksymalna
+    wody.Proszę wprowadzić do programu informacje o kilku wybranych gatunkach i za pomocą odpowiedniej klasy
+    implementującej interfejs Comparator rybki posortować w kolejności alfabetycznej wg. nazwy polskiej, alfabetycznej
+    wg. nazwy łacińskiej, rosnącej długości, malejącej temperatury minimalnej i rosnącej temperatury maksymalnej.
+    Oczywiście proszę o zaprezentowanie wyników tych sortowań. Warto zadbać o to aby gatunki wprowadzone do programu
+    prowadziły do innej kolejności rybek po zmianie kategorii.
+*/
+
+public class Fish {
+
+}
