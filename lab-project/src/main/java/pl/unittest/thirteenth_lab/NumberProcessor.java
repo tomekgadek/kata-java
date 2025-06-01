f9f00437f95dad3496d7fedda866735f2a8683a4
tomekgadek <gadektomek@gmail.com>
2025-06-01 13:32:06 +0200

    kata-java, przygotowanie tresci zadania (Number processor)

---
+++ lab-project/src/main/java/pl/unittest/thirteenth_lab/NumberProcessor.java
@@ -0,0 +1,23 @@
+package pl.unittest.thirteenth_lab;
+
+/*
+    Stwórz aplikację okienkową w Javie z wykorzystaniem bibliotek Swing i AWT.
+    Program powinien umożliwiać wykonanie operacji na liczbie całkowitej wprowadzonej przez użytkownika.
+
+    Interfejs graficzny powinien zawierać:
+    - jedno pole tekstowe do wprowadzania liczby,
+    - trzy przyciski:
+        1. "Zapisz wspak" – odwraca cyfry liczby i wyświetla wynik,
+        2. "Zamień na ósemkowy" – konwertuje liczbę na system ósemkowy i wyświetla wynik,
+        3. "Wyczyść" – czyści zawartość pola tekstowego,
+    - dwie etykiety:
+        - jedna z napisem "Wynik operacji:",
+        - druga do prezentacji wyniku działania.
+*/
+
+class NumberProcessor {
+
+    public static void main(String[] args) {
+        System.out.println("NumberProcessor");
+    }
+}
