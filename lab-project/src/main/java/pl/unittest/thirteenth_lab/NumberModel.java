c42d30f98a365affb708b7c784fa07163d54201a
tomekgadek <gadektomek@gmail.com>
2025-06-04 21:23:05 +0200

    kata-java, implementacja metody odpowiedzialnej za konwersje liczby do systemu osemkowego

--- lab-project/src/main/java/pl/unittest/thirteenth_lab/NumberModel.java
+++ lab-project/src/main/java/pl/unittest/thirteenth_lab/NumberModel.java
@@ -21,4 +21,15 @@ class NumberModel {
         return reversed;
     }
 
+
+    public int toOctal(int number) {
+        int octal = 0;
+        int multiplier = 1;
+        while (number != 0) {
+            octal += (number % 8) * multiplier;
+            multiplier *= 10;
+            number /= 8;
+        }
+        return octal;
+    }
 }
